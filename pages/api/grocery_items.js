57ffeeeb0691bae7792eb8ac275b1934d1da2f92
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-05-31 17:30:14 +0200

    Log MONGO_URL env var

--- pages/api/grocery_items.js
+++ pages/api/grocery_items.js
@@ -1,6 +1,7 @@
 import { GroceryItem } from "../../models"
 
 export default async (req, res) => {
+    console.log(process.env.MONGO_URL)
     const groceryItems = await GroceryItem.find();
 
     res.status(200).json(groceryItems)
