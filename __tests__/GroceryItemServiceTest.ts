a1fd61afc27b238cd9901ffcde7498350fc3b598
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-06-15 22:59:54 +0200

    Use jest with typescript

---
+++ __tests__/GroceryItemServiceTest.ts
@@ -0,0 +1,8 @@
+import dbConnect from '../lib/dbConnect'
+import GroceryItemService from '../services/GroceryItemService'
+
+describe('GroceryItemServiceTest', () => {
+    test("it should save new grocery item", () => {
+        let groceryItemService = new GroceryItemService()
+    })
+})
