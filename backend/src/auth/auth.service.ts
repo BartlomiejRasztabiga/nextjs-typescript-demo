2be96da74925f554764c4ad72633807f29e7e2e1
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-08-07 19:35:42 +0200

    Add LocalStrategy for passport

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
+import bcrypt from 'bcryptjs';
+
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users//users.service';
+import { User } from '../users/schemas/user.schema'
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService: UsersService) { }
+
+    async validateUser(email: string, password: string): Promise<User | null> {
+        const user = await this.usersService.findByEmail(email);
+        if (user) {
+            bcrypt.compare(password, user.password).then(response => {
+                if (response !== true) {
+                    return null;
+                }
+                const { password, ...result } = user;
+                return result
+            });
+        }
+
+
+        return null;
+    }
+}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -7,7 +7,6 @@ import { UsersService } from './users.service';
 @Module({
     imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
     providers: [UsersService],
-    // controllers: [GroceryItemsController],
-    // providers: [GroceryItemsService],
+    exports: [UsersService]
 })
 export class UsersModule { }
