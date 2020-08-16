bd834cdfb705f2d6d59a12cf6e896f1667193bb8
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-08-16 19:53:44 +0200

    Add /me endpoint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Req, Post, UseGuards } from '@nestjs/common';
+import { Controller, Req, Post, UseGuards, Get } from '@nestjs/common';
 import { Request } from 'express';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { User } from '../users/schemas/user.schema';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 
 @Controller('auth')
@@ -14,5 +15,11 @@ export class AuthController {
   @Post('login')
   async login(@Req() req: Request): Promise<User> {
     return this.authService.login(req.user as User);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@Req() req: Request) {
+    return req.user;
   }
 }
