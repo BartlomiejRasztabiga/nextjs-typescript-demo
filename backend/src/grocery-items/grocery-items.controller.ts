0e958441378f9a66f2ec823f3b969607f44c05dd
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-08-16 21:57:11 +0200

    Make frontend work with new backend

--- backend/src/grocery-items/grocery-items.controller.ts
+++ backend/src/grocery-items/grocery-items.controller.ts
@@ -17,6 +17,8 @@ export class GroceryItemsController {
     @Get()
     @UseGuards(JwtAuthGuard)
     async getAll(): Promise<GroceryItem[]> {
+        //TODO https://docs.nestjs.com/techniques/serialization
+        //Don't return not wanted fields
         return this.groceryItemsService.findAll();
     }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors();
+  // app.use(csurf()); //TODO Enable when added support for CSRF on the frontend
   //TODO add specific config for production grade environments
-  await app.listen(3000);
+  await app.listen(9000);
 }
 bootstrap();
