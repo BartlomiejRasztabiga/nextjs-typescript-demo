1d1ef80600337841d29648011c406019d0cb7e97
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-08-16 20:11:48 +0200

    Add custom JWT guard

--- backend/src/auth/local.strategy.ts
+++ backend/src/auth/local.strategy.ts
@@ -2,21 +2,26 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ModuleRef, ContextIdFactory } from '@nestjs/core';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
+    constructor(private moduleRef: ModuleRef) {
         super(
             {
                 usernameField: 'email',
                 passwordField: 'password',
                 session: false,
-                // passReqToCallback: true
+                passReqToCallback: true
             });
     }
 
-    async validate(email: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(email, password);
+    async validate(request: Request, email: string, password: string): Promise<any> {
+        const contextId = ContextIdFactory.getByRequest(request);
+        // "AuthService" is a request-scoped provider
+        const authService = await this.moduleRef.resolve(AuthService, contextId);
+
+        const user = await authService.validateUser(email, password);
         if (!user) {
             throw new UnauthorizedException();
         }
