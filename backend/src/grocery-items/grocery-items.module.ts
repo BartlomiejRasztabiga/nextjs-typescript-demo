3bf5f36a649b924a7df463103fde5ada27a65d6a
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-08-16 20:47:31 +0200

    Require JWT for grocery items controller

--- backend/src/grocery-items/grocery-items.module.ts
+++ backend/src/grocery-items/grocery-items.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { GroceryItemsController } from './grocery-items.controller';
 import { GroceryItemsService } from './grocery-items.service';
 import { GroceryItem, GroceryItemSchema } from './schemas/grocery-item.schema';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: GroceryItem.name, schema: GroceryItemSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: GroceryItem.name, schema: GroceryItemSchema }]),
+        PassportModule.register({ defaultStrategy: 'jwt' })
+    ],
     controllers: [GroceryItemsController],
     providers: [GroceryItemsService],
 })
